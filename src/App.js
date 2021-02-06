5b06b413c0b3255f312d457389477a64341f2595
pratikshit123 <pratikshitpfdpython1@gmail.com>
2021-02-07 00:33:36 +0530

    Todo functionality implemented,still 25% remaining,will work on dark mode feature next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Image from "./images/bg-desktop-dark.jpg";
 
-function App() {
+function AddTodo({ con }) {
+  const [click, setClick] = React.useState(false)
+  return (
+    <div style={{ color: "white", borderBottom: " 1px grey solid",padding:"20px" }}>
+      <div style={{ textAlign: "left" }}>
+        <input type="checkbox" value={con.text} onClick={(e) => setClick(true)} className="checkbox"></input>
+        {click === false ? (<label style={{ color: "grey", marginLeft: "15px" }}>{con.text}</label>):( <label style={{ color: "grey", marginLeft: "15px" }}><del>{con.text}</del></label>)}
+    </div>
+    </div>
+  );
+}
+
+function Todo() {
+  const [todo, setTodo] = React.useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo(todo);
+    setTodo("");
+  };
+  const [count, setCount] = React.useState([]);
+  const [text, setText] = React.useState("");
+
+  const addTodo = (text) => {
+    setCount([...count, { text }]);
+  };
+
+  return (
+    <div className="todo-main-section">
+      <h1 className="header-text">TODO</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input-area"
+          todo={todo}
+          placeholder="Create a new todo...."
+          onChange={(e) => setTodo(e.target.value)}
+        />
+      </form>
+      <div className="todos">
+        {count.map((con, index) => (
+          <AddTodo con={con} index={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function App({ addTodo }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="background-img" src={Image} />
+      <Todo addTodo={addTodo} />
     </div>
   );
 }
