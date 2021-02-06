ddc5f7ba2c4b5de4a347d8120b4b350884043516
Pratikshit26 <pratikshitpfd@gmail.com>
2021-02-07 03:02:17 +0530

    added todo feature and delete functionality for clear completed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
 import React, { useState } from "react";
-import "./App.css";
+import "./App.scss";
 import Image from "./images/bg-desktop-dark.jpg";
+import Image2 from "./images/sun.png";
+import useDarkMode from 'use-dark-mode';
 
-function AddTodo({ con }) {
-  const [click, setClick] = React.useState(false)
+function AddTodo({ con, setNum, num }) {
+  const [click, setClick] = React.useState(false);
+  const handleChecked = () => {
+    setClick(true);
+    setNum(num - 1);
+  };
+
   return (
-    <div style={{ color: "white", borderBottom: " 1px grey solid",padding:"20px" }}>
-      <div style={{ textAlign: "left" }}>
-        <input type="checkbox" value={con.text} onClick={(e) => setClick(true)} className="checkbox"></input>
-        {click === false ? (<label style={{ color: "grey", marginLeft: "15px" }}>{con.text}</label>):( <label style={{ color: "grey", marginLeft: "15px" }}><del>{con.text}</del></label>)}
-    </div>
+    <div className="todo-view-section">
+      <div className="todo-list-div">
+        <input
+          type="checkbox"
+          value={con.text}
+          onClick={handleChecked}
+          className="checkbox"
+        />
+        {click === false ? (
+          <label className="todo-list-label">{con.text}</label>
+        ) : (
+          <label className="todo-list-label">
+            <del>{con.text}</del>
+          </label>
+        )}
+      </div>
     </div>
   );
 }
 
 function Todo() {
   const [todo, setTodo] = React.useState("");
+  const [num, setNum] = React.useState(0);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!todo) return;
     addTodo(todo);
     setTodo("");
+    setNum(num + 1);
   };
   const [count, setCount] = React.useState([]);
-  const [text, setText] = React.useState("");
-
   const addTodo = (text) => {
     setCount([...count, { text }]);
   };
-
+  const clearList = () => {
+    setCount([""]);
+    setNum(0);
+  };
+    const darkMode = useDarkMode(false);
+  
   return (
     <div className="todo-main-section">
-      <h1 className="header-text">TODO</h1>
+      <div className="header">
+        <h1 className="header-text">TODO</h1>
+        <img className="dark-mode-icon" height={36} onClick={darkMode.toggle} src={Image2} />
+      </div>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -43,8 +69,14 @@ function Todo() {
       </form>
       <div className="todos">
         {count.map((con, index) => (
-          <AddTodo con={con} index={index} />
+          <AddTodo con={con} index={index} setNum={setNum} num={num} />
         ))}
+        <div className="todo-list-footer">
+          <span className="total-todo-left">{num} items left</span>
+          <span onClick={clearList} style={{ textAlign: "right" }}>
+            clear completed
+          </span>
+        </div>
       </div>
     </div>
   );
